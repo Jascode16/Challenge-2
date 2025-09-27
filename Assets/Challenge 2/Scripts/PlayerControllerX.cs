8e3aede1fcd0b0eea45d2979f7cea7201113877d
Jascode16 <Jascode16@gmail.com>
2025-09-27 12:58:56 +0100

    first commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogSpawnInterval = 1f;
+    private float lastSpawnTime = 0;
+    private bool canSpawnDog = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastSpawnTime > dogSpawnInterval)
+        {
+            canSpawnDog = true;
+        }
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawnDog)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawnTime = Time.time;
+            canSpawnDog = false;
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float minSpawnInterval = 3.0f;
+    private float maxSpawnInterval = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+    // Generate random ball index and random spawn position
+    Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+    // Instantiate ball at random spawn location
+    int ballIndex = Random.Range(0, ballPrefabs.Length);
+    Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+    // Schedule next spawn at a random interval between 3 and 5 seconds
+    float randomInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+    // Debug.Log("Next ball in: " + randomInterval + " seconds");
+    Invoke("SpawnRandomBall", randomInterval);
+    }
+
+}
